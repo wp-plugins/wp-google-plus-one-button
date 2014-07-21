02e909dafcdacc5725ee5e5bcd8646238b01cf90
vivacityinfotech.jaipur <vivacityinfotech.jaipur@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-21 09:53:25 +0000

    Added Lanuage option in plugin settings to show button on native language
    git-svn-id: https://plugins.svn.wordpress.org/wp-google-plus-one-button/trunk@952083 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-google-plus-one-button.php
+++ wp-google-plus-one-button.php
@@ -3,7 +3,7 @@
  * Plugin Name:WP Google Plus One Button
  * Plugin URI: http://vivacityinfotech.net
  * Description: A simple Google Plus Like and Share Button plugin for your posts/pages or Home page in your own language.
- * Version: 1.1
+ * Version: 1.2
  * Author: Vivacity Infotech Pvt. Ltd.
  * Author URI: http://vivacityinfotech.net
  * License: GPL2
@@ -130,8 +130,13 @@ function viva_create_google_gui(){
 		global $enable_plugin, $placing_loc,$icon_size,$language,$width,$data_ann;
 		
 		$above='';$below='';$small=''; $medium=''; $standard=''; $tall='';
-		$ar = ''; $zhHK = ''; $enGB = ''; $en = ''; $fr = ''; $de = ''; $hi = ''; $it = ''; $ko = ''; $ptPT = '';
-		$ru = ''; $es = ''; $sv = ''; $tr = ''; $uk = ''; $ur = '';
+		$af= ''; $am= ''; $ar = ''; $eq=''; $bn= ''; $bg= ''; $ca= ''; $zhHK = '';$zhCN= ''; $zhTW= ''; $hr= '';
+		$cs= ''; $da= ''; $ni= ''; $enGB = ''; $en = ''; $et= ''; $fil= ''; $fi= ''; $frCA= ''; $fr = ''; 
+		$gl= '';	$de = ''; $el= ''; $gu= ''; $iw= ''; $hi = ''; $hu= ''; $is= ''; $id= ''; $it = ''; $ja= ''; $kn= '';
+		$ko = ''; $lv= ''; $lt= ''; $ms= ''; $ml= ''; $mr= ''; $no= ''; $fa= ''; $pl= ''; $ptPR= ''; $ptPT = '';
+		$ro= ''; $ru = ''; $sr= ''; $sk= ''; $sl= ''; $es419= ''; $es = ''; $sw= ''; $sv= ''; $ta= ''; $te= '';
+		$th= ''; $tr= ''; $uk = ''; $ur= ''; $vi= ''; $zu= '';
+		
 		if($enable_plugin == true)
 			$check=' checked=checked';
 		else
@@ -152,39 +157,128 @@ function viva_create_google_gui(){
 		else if($icon_size == 'tall')
 			$tall = ' selected=selected'; 
 			
-				
+		if($language == 'af')
+					$af = ' selected=selected'; 
+		if($language == 'am')
+					$am = ' selected=selected'; 
 		if($language == 'ar')
 					$ar = ' selected=selected'; 
+		if($language == 'eq')
+					$eq = ' selected=selected'; 
+		if($language == 'bn')
+					$bn = ' selected=selected'; 
+		if($language == 'bg')
+					$bg = ' selected=selected'; 
+		if($language == 'ca')
+					$ca = ' selected=selected'; 
 		if($language == 'zh-HK')
 					$zhHK = ' selected=selected'; 
+		if($language == 'zh-CN')
+					$zhCN = ' selected=selected'; 
+		if($language == 'zh-TW')
+					$zhTW = ' selected=selected'; 
+		if($language == 'hr')
+					$hr = ' selected=selected'; 
+		if($language == 'cs')
+					$cs = ' selected=selected'; 
+		if($language == 'da')
+					$da = ' selected=selected'; 
+		if($language == 'ni')
+					$ni = ' selected=selected'; 
 		if($language == 'en-GB')
 					$enGB = ' selected=selected'; 
 		if($language == 'en')
 					$en = ' selected=selected'; 
+		if($language == 'et')
+					$et = ' selected=selected'; 
+		if($language == 'fil')
+					$fil = ' selected=selected'; 
+		if($language == 'fi')
+					$fi = ' selected=selected'; 
+		if($language == 'fr-CA')
+					$frCA = ' selected=selected'; 
 		if($language == 'fr')
 					$fr = ' selected=selected'; 
+		if($language == 'gl')
+					$gl = ' selected=selected'; 
 		if($language == 'de')
 					$de = ' selected=selected'; 
+		if($language == 'el')
+					$el = ' selected=selected'; 
+		if($language == 'de')
+					$de = ' selected=selected'; 
+		if($language == 'iw')
+					$iw = ' selected=selected'; 
 		if($language == 'hi')
 					$hi = ' selected=selected'; 
+		if($language == 'hu')
+					$hu = ' selected=selected'; 
+		if($language == 'is')
+					$is = ' selected=selected'; 
+		if($language == 'id')
+					$id = ' selected=selected'; 
 		if($language == 'it')
 					$it = ' selected=selected'; 
+		if($language == 'ja')
+					$ja = ' selected=selected'; 
+		if($language == 'kn')
+					$kn = ' selected=selected'; 
 		if($language == 'ko')
 					$ko = ' selected=selected'; 
+		if($language == 'lv')
+					$lv = ' selected=selected'; 
+		if($language == 'lt')
+					$lt = ' selected=selected'; 
+		if($language == 'ms')
+					$ms = ' selected=selected'; 
+		if($language == 'ml')
+					$ml = ' selected=selected'; 
+		if($language == 'mr')
+					$mr = ' selected=selected'; 
+		if($language == 'no')
+					$no = ' selected=selected'; 
+		if($language == 'fa')
+					$fa = ' selected=selected'; 
+		if($language == 'pl')
+					$pl = ' selected=selected'; 
+		if($language == 'pt-PR')
+					$ptPR = ' selected=selected'; 
 		if($language == 'pt-PT')
 					$ptPT = ' selected=selected'; 
+		if($language == 'ro')
+					$ro = ' selected=selected'; 
 		if($language == 'ru')
 					$ru = ' selected=selected'; 
+		if($language == 'sr')
+					$sr = ' selected=selected'; 
+		if($language == 'sk')
+					$sk = ' selected=selected'; 
+		if($language == 'sl')
+					$sl = ' selected=selected'; 
+		if($language == 'es419')
+					$es419 = ' selected=selected'; 
 		if($language == 'es')
 					$es = ' selected=selected'; 
+		if($language == 'sw')
+					$sw = ' selected=selected'; 
 		if($language == 'sv')
 					$sv = ' selected=selected'; 
+		if($language == 'ta')
+					$ta = ' selected=selected'; 
+		if($language == 'te')
+					$te = ' selected=selected'; 
+		if($language == 'th')
+					$th = ' selected=selected'; 
 		if($language == 'tr')
 					$tr = ' selected=selected'; 
 		if($language == 'uk')
 					$uk = ' selected=selected'; 
 		if($language == 'ur')
 					$ur = ' selected=selected'; 
+		if($language == 'vi')
+					$vi = ' selected=selected'; 
+		if($language == 'zu')
+					$zu = ' selected=selected'; 
 					
 		$inline=''; $bubble=''; $none='';
 		if($data_ann == 'inline')
@@ -239,22 +333,67 @@ function viva_create_google_gui(){
 									<td>'.__('Language settings', 'wp-gpob-plugin' ).'</td>
 									<td>
 										<select name="lang">
-											<option value="ar" '.$ar.'>'.__('Arabic', 'wp-gpob-plugin' ).'</option>
-											<option value="zh-HK" '.$zhHK.'>'.__('Chinese', 'wp-gpob-plugin' ).'</option>
+											<option value="af" '.$af.'>'.__('Afrikaans', 'wp-gpob-plugin' ).'</option>
+											<option value="am" '.$am.'>'.__('Amharic - ‪አማርኛ‬', 'wp-gpob-plugin' ).'</option>
+											<option value="ar" '.$ar.'>'.__('Arabic - ‫العربية‬', 'wp-gpob-plugin' ).'</option>
+											<option value="eq" '.$eq.'>'.__('Basque - ‪euskara‬', 'wp-gpob-plugin' ).'</option>
+											<option value="bn" '.$bn.'>'.__('Bengali - ‪বাংলা‬', 'wp-gpob-plugin' ).'</option>
+											<option value="bg" '.$bg.'>'.__('Bulgarian - ‪български‬', 'wp-gpob-plugin' ).'</option>
+											<option value="ca" '.$ca.'>'.__('Catalan - ‪català‬', 'wp-gpob-plugin' ).'</option>
+											<option value="zh-HK" '.$zhHK.'>'.__('Chinese (Hong Kong) - ‪中文（香港）‬', 'wp-gpob-plugin' ).'</option>
+											<option value="zh-CN" '.$zhCN.'>'.__('Chinese (Simplified) - ‪简体中文‬', 'wp-gpob-plugin' ).'</option>
+											<option value="zh-TW" '.$zhTw.'>'.__('Chinese (Traditional) - ‪繁體中文‬', 'wp-gpob-plugin' ).'</option>
+											<option value="hr" '.$hr.'>'.__('Croatian - ‪Hrvatski‬', 'wp-gpob-plugin' ).'</option>
+											<option value="cs" '.$cs.'>'.__('Czech - ‪Čeština‬', 'wp-gpob-plugin' ).'</option>
+											<option value="da" '.$da.'>'.__('Danish - ‪Dansk‬', 'wp-gpob-plugin' ).'</option>
+											<option value="ni" '.$ni.'>'.__('Dutch - ‪Nederlands‬', 'wp-gpob-plugin' ).'</option>
 											<option value="en-GB" '.$enGB.'>'.__('English (United Kingdom)', 'wp-gpob-plugin' ).'</option>
 											<option value="en" '.$en.'>'.__('English (USA)', 'wp-gpob-plugin' ).'</option>
+											<option value="et" '.$et.'>'.__('Estonian - ‪eesti‬', 'wp-gpob-plugin' ).'</option>
+											<option value="fil" '.$fil.'>'.__('Filipino', 'wp-gpob-plugin' ).'</option>
+											<option value="fi" '.$fi.'>'.__('Finnish - ‪Suomi‬', 'wp-gpob-plugin' ).'</option>
+											<option value="fr-CA" '.$frCA.'>'.__('French (Canada) - ‪Français (Canada)‬', 'wp-gpob-plugin' ).'</option>
 											<option value="fr" '.$fr.'>'.__('French (France) - ‪Français (France)', 'wp-gpob-plugin' ).'</option>
+											<option value="gl" '.$gl.'>'.__('Galician - ‪galego‬', 'wp-gpob-plugin' ).'</option>
 											<option value="de" '.$de.'>'.__('German -Deutsch', 'wp-gpob-plugin' ).'</option>
+											<option value="el" '.$el.'>'.__('Greek - ‪Ελληνικά‬', 'wp-gpob-plugin' ).'</option>
+											<option value="gu" '.$gu.'>'.__('Gujarati - ‪ગુજરાતી‬', 'wp-gpob-plugin' ).'</option>
+											<option value="iw" '.$iw.'>'.__('Hebrew - ‫עברית‬', 'wp-gpob-plugin' ).'</option>
 											<option value="hi" '.$hi.'>'.__('Hindi - ‪हिन्दी', 'wp-gpob-plugin' ).'</option>
+											<option value="hu" '.$hu.'>'.__('Hungarian - ‪magyar‬', 'wp-gpob-plugin' ).'</option>
+											<option value="is" '.$is.'>'.__('Icelandic - ‪íslenska‬', 'wp-gpob-plugin' ).'</option>
+											<option value="id" '.$id.'>'.__('Indonesian - ‪Bahasa Indonesia‬', 'wp-gpob-plugin' ).'</option>
 											<option value="it" '.$it.'>'.__('Italian - ‪Italiano', 'wp-gpob-plugin' ).'</option>
+											<option value="ja" '.$ja.'>'.__('Japanese - ‪日本語‬', 'wp-gpob-plugin' ).'</option>
+											<option value="kn" '.$kn.'>'.__('Kannada - ‪ಕನ್ನಡ‬', 'wp-gpob-plugin' ).'</option>
 											<option value="ko" '.$ko.'>'.__('Korean - ‪한국어', 'wp-gpob-plugin' ).'</option>
+											<option value="lv" '.$lv.'>'.__('Latvian - ‪latviešu‬', 'wp-gpob-plugin' ).'</option>
+											<option value="lt" '.$lt.'>'.__('Lithuanian - ‪lietuvių‬', 'wp-gpob-plugin' ).'</option>
+											<option value="ms" '.$ms.'>'.__('Malay - ‪Bahasa Melayu‬', 'wp-gpob-plugin' ).'</option>
+											<option value="ml" '.$ml.'>'.__('Malayalam - ‪മലയാളം‬‬', 'wp-gpob-plugin' ).'</option>
+											<option value="mr" '.$mr.'>'.__('Marathi - ‪मराठी‬‬', 'wp-gpob-plugin' ).'</option>
+											<option value="no" '.$no.'>'.__('Norwegian - ‪norsk‬', 'wp-gpob-plugin' ).'</option>
+											<option value="fa" '.$fa.'>'.__('Persian - ‫فارسی‬‬', 'wp-gpob-plugin' ).'</option>
+											<option value="pl" '.$pl.'>'.__('Polish - ‪polski‬‬', 'wp-gpob-plugin' ).'</option>
+											<option value="pt-PR" '.$ptPR.'>'.__('Portuguese (Brazil) - ‪Português (Brasil)‬‬‬', 'wp-gpob-plugin' ).'</option>
 											<option value="pt-PT" '.$ptPT.'>'.__('Portuguese (Portugal) - ‪Português (Portugal)', 'wp-gpob-plugin' ).'</option>
+											<option value="ro" '.$ro.'>'.__('Romanian - ‪română‬', 'wp-gpob-plugin' ).'</option>
 											<option value="ru" '.$ru.'>'.__('Russian - ‪Русский‬', 'wp-gpob-plugin' ).'</option>
+											<option value="sr" '.$sr.'>'.__('Serbian - ‪Српски‬', 'wp-gpob-plugin' ).'</option>
+											<option value="sk" '.$sk.'>'.__('Slovak - ‪Slovenčina‬‬', 'wp-gpob-plugin' ).'</option>
+											<option value="sl" '.$sl.'>'.__('Slovenian - ‪slovenščina‬‬', 'wp-gpob-plugin' ).'</option>
+											<option value="es-419" '.$es419.'>'.__('Spanish (Latin America) - ‪Español (Latinoamérica)‬', 'wp-gpob-plugin' ).'</option>
 											<option value="es" '.$es.'>'.__('Spanish (Spain) - ‪Español (España)', 'wp-gpob-plugin' ).'</option>
+											<option value="sw" '.$sw.'>'.__('Swahili - ‪Kiswahili‬', 'wp-gpob-plugin' ).'</option>
 											<option value="sv" '.$sv.'>'.__('Swedish - ‪Svenska‬', 'wp-gpob-plugin' ).'</option>
+											<option value="ta" '.$ta.'>'.__('Tamil - ‪தமிழ்‬', 'wp-gpob-plugin' ).'</option>
+											<option value="te" '.$te.'>'.__('Telugu - ‪తెలుగు‬', 'wp-gpob-plugin' ).'</option>
+											<option value="th" '.$th.'>'.__('Thai - ‪ไทย‬', 'wp-gpob-plugin' ).'</option>
 											<option value="tr" '.$tr.'>'.__('Turkish - ‪Türkçe', 'wp-gpob-plugin' ).'</option>
 											<option value="uk" '.$uk.'>'.__('Ukrainian - ‪Українська', 'wp-gpob-plugin' ).'</option>
 											<option value="ur" '.$ur.'>'.__('Urdu - ‫اردو‬', 'wp-gpob-plugin' ).'</option>
+											<option value="vi" '.$vi.'>'.__('Vietnamese - ‪Tiếng Việt‬', 'wp-gpob-plugin' ).'</option>
+											<option value="zu" '.$zu.'>'.__('Zulu - ‪isiZulu‬', 'wp-gpob-plugin' ).'</option>
 
 										</select>
 									</td>
